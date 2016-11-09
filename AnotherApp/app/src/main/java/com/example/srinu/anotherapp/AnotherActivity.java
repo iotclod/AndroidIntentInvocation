a67c3c1f3173b1b60df65c7ce4674f4c6266130b
iotlabuser1 <iotlabuser1>
2016-11-09 13:28:38 +0000

    adding files

---
+++ AnotherApp/app/src/main/java/com/example/srinu/anotherapp/AnotherActivity.java
@@ -0,0 +1,17 @@
+package com.example.srinu.anotherapp;
+
+/**
+ * Created by srinu on 11/4/2016.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnotherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
